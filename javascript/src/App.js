d9a2d464828d5a83b47724ad4041e9f412968260
Fareed Javed <fareedjaved@gmail.com>
2018-03-20 21:36:10 -0500

    adding front end

---
+++ javascript/src/App.js
@@ -0,0 +1,50 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+
+class FoodList extends Component {
+  constructor(props) {
+    super(props); 
+    this.state = {
+      foodList: {},
+    }
+  }
+
+  render() {
+    return(
+    <div>
+      <ul>
+        {this.state.foodList.forEach((val,key,map) => 
+          <li>{key} from {val}</li>
+        )}
+      </ul> 
+      <button>Add Item</button>
+    </div>
+    );
+  }
+}
+
+class App extends Component {
+
+  handleFormSubmit(e) {
+    e.preventDefault(); 
+  }
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">Welcome to React</h1>
+        </header>
+
+        <FoodList/>
+        <form onSubmit={this.handleFormSubmit}>
+          
+        </form>
+      </div>
+    );
+  }
+}
+
+export default App;
