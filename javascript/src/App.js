5f8b2fb89b6d90c26c9fb3e805bfac922b4076d9
Fareed Javed <fareedjaved@gmail.com>
2018-04-01 14:25:24 -0500

    rendered list and added delete functionality

--- javascript/src/App.js
+++ javascript/src/App.js
@@ -1,24 +1,80 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 import {FoodList} from './FoodList'; 
-
 // import request from 'request'; 
-
-
 
 class App extends Component {
 
+  constructor(props) {
+    super(props); 
+
+    this.state = {
+      foodItem: '', 
+      manufacturer:'',
+      foodList: []
+    }
+  }
+
+  // takes a list and an item and vendor 
+  // returns a list with the first 
+  remove = (list, date ) =>  {
+    return list.filter(e => e.id !== date)
+  }
+  // will take the key and value that need to be deleted 
+  // from the map 
+  handleDelete = (id) => {
+    let listAfterDelete = this.remove(this.state.foodList, id)
+    this.setState({
+      foodList: listAfterDelete
+    })
+    console.log("deleting item from map")
+  }
+
+  componentDidUpdate() {
+    console.log(this.state.foodList)
+  }
+  // handles the form being submitted 
+  handleFormSubmit = (e) => {
+    e.preventDefault();
+
+    const newFoodItem = {
+      item: this.state.foodItem,
+      vendor: this.state.manufacturer,
+      id: Date.now()
+    }
+
+    this.setState(prevState => ({
+      foodItem: '',
+      manufacturer: '',
+      foodList: prevState.foodList.concat(newFoodItem)
+    }))
+
+  }
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
+    <div>
 
-        <FoodList/>
-      </div>
+      <FoodList 
+        foodList={this.state.foodList} 
+        handleDelete={(id) => this.handleDelete(id)} 
+      />
+
+      <form onSubmit={(e) => {this.handleFormSubmit(e)} }>
+          Food Item: 
+          <input type="text" value={this.state.foodItem} 
+            onChange={(e) => this.setState({foodItem: e.target.value})}
+          /> 
+          <br/> 
+          Manufacturer:  
+          <input type="text" value={this.state.manufacturer} 
+            onChange={(e) => this.setState({manufacturer: e.target.value})}
+          />
+          <br/> 
+          {/* Submit Button */ }
+          <input type="submit" name="Submit" /> 
+        </form>
+    </div>
     );
   }
 }
--- javascript/src/FoodList.js
+++ javascript/src/FoodList.js
@@ -1,64 +1,29 @@
 import React, { Component } from 'react';
 
-// Displays the list of food items. 
-// Also renders a form for food input 
-// and will allow for items to be deleted   
-export class FoodList extends Component {
-    constructor(props) {
-      super(props); 
-      this.state = {
-        foodItem: '', 
-        manufacturer:'', 
-        foodList: new Map()
-      }
-    }
-  
-    // will take the key and value that need to be deleted 
-    // from the map 
-    handleDelete = (key, val) => {
-      console.log("deleting item from map")
-    }
-  
-    // handles the form being submitted 
-    handleFormSubmit = (e) => {
-      let newFoodList = this.state.foodList.set(this.foodItem, this.manufacturer);
-      this.setState({
-        foodItem: '',
-        manufacturer: '',
-        foodList: newFoodList
-      })
-      e.preventDefault(); 
-      console.log(newFoodList)
-    }
-  
-    render() {
-      return (
-      <div>
-        <ul>
-          {this.state.foodList.forEach((val,key,map) => 
-            <div>
-              <li>{key} from {val}</li>
-              <button onClick={() => this.handleDelete(key,val)}>Delete</button>
-            </div>  
-          )}
-        </ul> 
-        <form onSubmit={(e) => {this.handleFormSubmit(e)} }>
-            Food Item: 
-            <input type="text" value={this.state.foodItem} 
-              onChange={(e) => this.setState({foodItem: e.target.value})}
-            /> 
-            <br/> 
-            Manufacturer:  
-            <input type="text" value={this.state.manufacturer} 
-              onChange={(e) => this.setState({manufacturer: e.target.value})}
-            />
-            <br/> 
-            {/* Submit Button */ }
-            <input type="submit" name="Submit" /> 
-          </form>
-      </div>
-      );
-    }
-  }
 
- 
+// Displays the list of food items.
+// Note: FoodItems is a list of objects, see App.js for definition  
+ export class FoodList extends Component {
+
+   render() {
+      
+     return (
+       <div>
+         <ul>
+         
+           {this.props.foodList.map((food) => (
+             <div key={food.id}>
+                <li> 
+                  {food.item} from {food.vendor}
+                </li>
+                <button onClick={() => {this.props.handleDelete(food.id)}}>
+                  delete
+                </button>
+             </div>
+           ))}
+           
+         </ul>
+      </div>
+     );
+   }
+ }
