ffe5c0ae9a949878df75592cbcc5b9c6d6b7981c
Fareed Javed <fareedjaved@gmail.com>
2018-04-10 23:21:53 -0500

    -getting key via url param -rendering json without domain object

--- journalapi/src/main/java/com/foodjournal/api/SearchController.java
+++ journalapi/src/main/java/com/foodjournal/api/SearchController.java
@@ -1,32 +1,28 @@
 package com.foodjournal.api;
 
 import java.net.URI;
-import java.util.concurrent.atomic.AtomicLong;
-
 import org.springframework.boot.web.servlet.error.ErrorController;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.client.RestTemplate;
-import java.util.Map; 
+import org.springframework.web.client.RestTemplate; 
 
 @RestController
 public class SearchController implements ErrorController {
 
-	// private final AtomicLong counter = new AtomicLong(); 
 	private static final String PATH = "/error";  
 	RestTemplate restTemplate = new RestTemplate();
 	
 	
 	@RequestMapping(value="/search", method=RequestMethod.GET, 
 			produces="application/json")
-	public String greeting(@RequestParam(value="name", 
-			defaultValue="World") String name) throws Exception {
+	public String greeting(@RequestParam(value="key", 
+			defaultValue="World") String key) throws Exception {
 		
 		String url = "https://api.nal.usda.gov/ndb/search/?format=json&"
-				+ "q=apple&sort=n&max=25&offset=0&api_key=QBPIfEtWnjhWXxAtBJun8QGEjkYVUfWvbjBqy1Vc"; 
+				+ "q=apple&sort=n&max=25&offset=0&api_key=" + key; 
 		URI uri = new URI(url); 
 		
 		
