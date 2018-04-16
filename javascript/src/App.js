82a7bc2f37b204e027adc4ede90649e095b930c5
Fareed Javed <fareedjaved@gmail.com>
2018-04-15 23:02:40 -0500

    Successfully rendering food groups

--- javascript/src/App.js
+++ javascript/src/App.js
@@ -1,9 +1,10 @@
 import React, { Component } from 'react';
 import './App.css';
 import {FoodList} from './FoodList'; 
-import Select, { Async } from 'react-select'; 
+import Select from 'react-select'; 
 import 'react-select/dist/react-select.css';
-import fetch from 'isomorphic-fetch'; 
+import axios from 'axios' ; 
+
 
 class App extends Component {
 
@@ -12,9 +13,10 @@ class App extends Component {
 
     this.state = {
       foodItem: '', 
-      manufacturer:'',
+      foodList: [],
       foodGroup: '',
-      foodList: []
+      foodGroupList:[],
+     
     }
   }
 
@@ -33,42 +35,48 @@ class App extends Component {
     console.log("deleting item from map")
   }
 
-  componentDidUpdate() {
-    console.log(this.state.foodList)
-  }
-  // handles the form being submitted 
-  handleFormSubmit = (e) => {
-    e.preventDefault();
-
-    const newFoodItem = {
-      item: this.state.foodItem,
-      vendor: this.state.manufacturer,
-      id: Date.now()
-    }
-
-    this.setState(prevState => ({
-      foodItem: '',
-      manufacturer: '',
-      foodList: prevState.foodList.concat(newFoodItem)
-    }))
-
-  }
-
-  // TODO: 
-  //  Call the spring backend for the FDA food items
+  // Calling the spring backend for the FDA food items
   getOptions = (input, callback) => {
-    let myFakeOptions = [{value: 'one', label: 'One'}]
-    setTimeout(() => {
-      callback(null, {
-        options: myFakeOptions,
-        // CAREFUL! Only set this to true when there are no more options,
-        // or more specific queries will not be sent to the server.
-        complete: true
-      });
-    }, 500);
+    if (!input) {
+			return Promise.resolve({ options: [] });
+    }
+    return axios.get(`http://localhost:8080/search?item=${input}`)
+      .then((response)=> response.json())
+      .then((json)=> {
+        console.log("I am inside getOptions")
+        return { options: json.list.item }
+      })
   };
 
+  // This will be used for the button that chooses 
+	// the particular food group. Whatver gets chosen here 
+	// should probably get integrated with getOptions since 
+	// we want that to be bundled together for the backend... 
+  getFoodGroups = (input) => {
+    if (!input) {
+			return Promise.resolve({ options: [] });
+    }
+    return axios.get('http://localhost:8080/foodgroup')
+      .then((response)=> {
+        console.log(response.data.list.item)
+        return {options: response.data.list.item}
+      }) 
+  }
+  onManufacturerChange = (input) => {
+    this.setState({
+      manufacturer: input,
+    })
+  }
+
+  onItemChange = (value, prevState) =>  {
+    this.setState({
+      
+      foodItem: prevState.concat(value),
+    });
+  }
+
   render() {
+    const AsyncComponent = Select.Async; 
     return (
     <div>
 
@@ -77,35 +85,23 @@ class App extends Component {
         handleDelete={(id) => this.handleDelete(id)} 
       />
 
-      <form onSubmit={(e) => {this.handleFormSubmit(e)} }>
-          Food Item: 
-          <input type="text" value={this.state.foodItem} 
-            onChange={(e) => this.setState({foodItem: e.target.value})}
-          /> 
-          <br/> 
-          Manufacturer:  
-          <input type="text" value={this.state.manufacturer} 
-            onChange={(e) => this.setState({manufacturer: e.target.value})}
-          />
-          <br/> 
-          {/* Submit Button */ }
-          <input type="submit" name="Submit" /> 
-        </form>
+      {/* User selects the food group that the items they ate belongs to */}
+      <h3>Select the food groups in which the things you ate belong to </h3>
+      <Select.Async multi={true} value={this.state.manufacturer} 
+        onChange={this.onManufacturerChange} 
+        onValueClicked={console.log("Clicked")}
+        valueKey="name" labelKey="name" 
+        loadOptions={this.getFoodGroups} 
+      /> 
 
-        {/* this is for what they ate */}
-        <Select
-          name="foodItem"
-          value={this.state.foodItem}
-          onChange = {(e)=> this.setState({foodItem: e.target.value})}
-
-        /> 
-
-        {/* This is for the food group */}
-        <Select 
-          name="foodGroup"
-          value={this.state.manufacturer}
-          onChange = {(e) => this.setState({foodGroup: e.target.value})}
-        /> 
+      {/* User selects what they ate */}
+      <br/> 
+      <h3>What did you eat today?</h3> 
+      <AsyncComponent multi={true} value={this.state.foodItem} 
+        onChange={this.onItemChange} loadOptions={this.getOptions}
+        backspaceRemoves={true} placeholder={"Food Item"} /> 
+      <br/> 
+    
     </div>
     );
   }
