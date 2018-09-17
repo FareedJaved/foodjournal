8d7615074e53d90e528a251fa96a0ae2fcf0a97e
Fareed Javed <fareedjaved@gmail.com>
2018-09-16 19:46:01 -0500

    Added tests for user model

--- foodjournal/journalapi/tests/test_models/test_user_model.py
+++ foodjournal/journalapi/tests/test_models/test_user_model.py
@@ -1,15 +1,18 @@
 from django.test import TestCase
 from journalapi.models import User 
+
 # Testing the User Model
 class UserModelTests(TestCase):
 
     def test_user_has_name(self):
         test_user = User()
         test_user.first_name = "Yucca"
-        test_user.last_name = "Momo
+        test_user.last_name = "Momo"
+        test_user.email = "test_doge@yahoo.com"
         test_user.save()
         self.assertIsNotNone(test_user.first_name)
         self.assertIsNotNone(test_user.last_name)
-    
-    def test_user_has_email(self):
-        test
+        self.assertIsNotNone(test_user.email)
+        self.assertIsNone(test_user.birthday)
+        self.assertIsNone(test_user.age)
+        self.assertIsNone(test_user.weight)
