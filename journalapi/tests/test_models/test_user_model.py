ae1da3dfe3fd0027c04d566cead04c11c98c7ace
Fareed Javed <fareedjaved@gmail.com>
2018-09-17 00:47:30 -0500

    Added primary key test for FoodEntry

--- journalapi/tests/test_models/test_user_model.py
+++ journalapi/tests/test_models/test_user_model.py
@@ -1,6 +1,6 @@
 from django.test import TestCase
-from journalapi.models import User 
-
+from journalapi.models import User, FoodEntry
+from django.utils import timezone
 # Testing the User Model
 class UserModelTests(TestCase):
 
@@ -16,3 +16,19 @@ class UserModelTests(TestCase):
         self.assertIsNone(test_user.birthday)
         self.assertIsNone(test_user.age)
         self.assertIsNone(test_user.weight)
+
+    def test_food_entry_has_user(self):
+        test_user = User() 
+        test_user.first_name = "Yucca"
+        test_user.last_name = "Momo"
+        test_user.email = "test_dodge@yahoo.com"
+        test_user.save()
+        tiempo = timezone.now()
+        test_foodentry = FoodEntry()
+        test_foodentry.food_item = "apple"
+        test_foodentry.amount_eaten = 3
+        test_foodentry.time_eaten = tiempo
+        test_foodentry.associated_user = test_user
+        test_foodentry.save()
+        self.assertEqual(test_user.pk, test_foodentry.associated_user.id)
+        
