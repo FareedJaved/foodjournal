24ccac273a1c9575b653b89a4d5403cf7aa53f76
Fareed Javed <fareedjaved@gmail.com>
2018-10-02 00:09:20 -0500

    Added a route for handling requests from frontend

--- foodjournal/urls.py
+++ foodjournal/urls.py
@@ -21,7 +21,8 @@ from foodjournal import views
 router = routers.DefaultRouter()
 router.register(r'users', views.UserViewSet)
 router.register(r'foodentries', views.FoodEntryViewSet)
-
+router.register(r'getfood', views.UserCountView)
+ 
 
 # Wire up our API using automatic URL routing.
 # Additionally, we include login URLs for the browsable API.
