b82443fb8cc77811994bf5d513f8087d40683461
Fareed Javed <fareedjaved@gmail.com>
2018-12-24 13:23:58 -0600

    Exposed login as an endpoint, cleaned up imports

--- foodjournal/views.py
+++ foodjournal/views.py
@@ -1,16 +1,30 @@
+from django.contrib.auth import login
+
+from rest_framework import viewsets, permissions
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.authtoken.serializers import AuthTokenSerializer
+
+from knox.auth import TokenAuthentication
+from knox.views import LoginView as KnoxLoginView
+
 from journalapi.models import User, FoodEntry, FoodGroups
-from rest_framework import viewsets, status
-from rest_framework.decorators import api_view
-from rest_framework.response import Response
 from foodjournal.serializers import UserSerializer, FoodEntrySerializer, FoodGroupsSerializer
-from requests import request 
-import os
-import requests
+
+
+class LoginView(KnoxLoginView):
+    permission_classes = (permissions.AllowAny,)
+
+    def post(self, request, format=None):
+        serializer = AuthTokenSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        user = serializer.validated_data['user']
+        login(request, user)
+        return super(LoginView, self).post(request, format=None)
 
 
 class UserViewSet(viewsets.ModelViewSet):
     """
-    API endpoint that allows users to be viewed or edited. 
+    API endpoint that allows users to be viewed or edited.
     """
     queryset = User.objects.all()
     serializer_class = UserSerializer
@@ -28,22 +42,20 @@ class FoodGroupsViewSet(viewsets.ModelViewSet):
     """
     A view that returns the food groups from the USDA database.
     """
-    
+
     queryset = FoodGroups.objects.all()
     serializer_class = FoodGroupsSerializer
-    # api_key = os.environ['USDA']
+    authentication_classes = (TokenAuthentication)
+    permission_classes = (IsAuthenticated)
 
+    # api_key = os.environ['USDA']
 
     # def get_food_groups(self):
     #     url = f'https://api.nal.usda.gov/ndb/list?format=json&lt=g&sort=n&api_key={self.api_key}'
     #     api_response = requests.get(url)
 
     #     return api_response
-    
+
     # def list(self, request, format=None):
-    #     food_group_query = self.get_food_groups() 
+    #     food_group_query = self.get_food_groups()
     #     return Response({"food_groups": food_group_query})
-        
-
-
-    
