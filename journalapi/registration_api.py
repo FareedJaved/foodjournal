745f22e72c9e0c72cf54e123cef847e9c79c74fe
Fareed Javed <fareedjaved@gmail.com>
2018-12-27 15:29:00 -0600

    Added registration view to create a user for first time

---
+++ journalapi/registration_api.py
@@ -0,0 +1,24 @@
+from rest_framework import viewsets, permissions, generics
+from rest_framework.response import Response
+
+from django.contrib.auth.models import User
+
+from knox.models import AuthToken
+
+from foodjournal.serializers import CreateUserSerialzer, UserSerializer
+
+
+class RegistrationAPI(generics.GenericAPIView):
+    serializer_class = CreateUserSerialzer
+    queryset = User.objects.all()
+
+    def post(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        user = serializer.save()
+
+        return Response({
+            "user": UserSerializer(user,
+                                   context=self.get_serializer_context()).data,
+            "token": AuthToken.objects.create(user)
+        })
