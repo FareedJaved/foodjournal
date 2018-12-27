ddcd9d34b4ba313a55acd25212a43ad1a06b9728
Fareed Javed <fareedjaved@gmail.com>
2018-12-27 15:21:50 -0600

    Added create_user serializer

--- foodjournal/serializers.py
+++ foodjournal/serializers.py
@@ -1,19 +1,36 @@
-from journalapi.models import User, FoodEntry, FoodGroups
 from rest_framework import serializers
+
+from journalapi.models import User, FoodEntry, FoodGroups
+
+
+class CreateUserSerialzer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ('id', 'username', 'password')
+        extra_kwargs = {'password': {'write_only': True}}
+
+    def create(self, validated_data):
+        user = User.objects.create_user(
+            validated_data['username'], None, validated_data['password'])
+
+        return user
 
 
 class UserSerializer(serializers.HyperlinkedModelSerializer):
-    class Meta: 
-        model = User 
-        fields = ('url', 'first_name', 'last_name', 'email', 'age', 'weight', 'birthday')
+    class Meta:
+        model = User
+        fields = ('id', 'first_name', 'last_name',
+                  'email', 'username')
 
 
 class FoodEntrySerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
-        model = FoodEntry 
-        fields = ('url', 'food_item', 'amount_eaten', 'time_eaten', 'associated_user')
+        model = FoodEntry
+        fields = ('url', 'food_item', 'amount_eaten',
+                  'time_eaten', 'associated_user')
 
-class FoodGroupsSerializer(serializers.HyperlinkedModelSerializer): 
-    class Meta: 
+
+class FoodGroupsSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
         model = FoodGroups
         fields = ('group_name', 'group_id')
