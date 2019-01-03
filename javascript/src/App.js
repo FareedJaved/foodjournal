3bc15ff514220e2b51d3021cdf9ceeb7bbe9581e
Fareed Javed <fareedjaved@gmail.com>
2019-01-02 23:58:59 -0600

    Added link to imports

--- javascript/src/App.js
+++ javascript/src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import "./App.css";
-import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 import SignUpForm from "./pages/SignUpForm";
 import SignInForm from "./pages/SignInForm";
 import FoodSearch from "./pages/FoodSearch";
