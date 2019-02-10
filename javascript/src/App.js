904d3f39075dba6898d2bdc152f98e2d120e17ae
Fareed Javed <fareedjaved@gmail.com>
2019-02-10 15:24:24 -0600

    Added protective route for home page

--- javascript/src/App.js
+++ javascript/src/App.js
@@ -1,9 +1,30 @@
 import React, { Component } from "react";
 import "./App.css";
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { BrowserRouter as Router, Route, Redirect } from "react-router-dom";
 import SignUpForm from "./pages/SignUpForm";
 import SignInForm from "./pages/SignInForm";
 import FoodEntryForm from "./pages/FoodEntryForm";
+import AuthService from "../src/lib/AuthService";
+
+// protecting the route to home page
+function PrivateRoute({ component: Component, ...rest }) {
+  return (
+    <Route
+      {...rest}
+      render={props =>
+        AuthService.isAuthenticated() ? (
+          <Component {...props} />
+        ) : (
+          <Redirect
+            to={{
+              pathname: "/"
+            }}
+          />
+        )
+      }
+    />
+  );
+}
 
 class App extends Component {
   render() {
@@ -12,11 +33,19 @@ class App extends Component {
         <div>
           <Route exact path="/" component={SignUpForm} />
           <Route path="/signin" component={SignInForm} />
-          <Route path="/home" component={FoodEntryForm} />
+          <PrivateRoute path="/home" component={FoodEntryForm} />
         </div>
       </Router>
     );
   }
 }
+
+// const PrivateRoute = ({ component: Component, ...rest }) => (
+//   <Route {...rest} render={() => (
+//     fakeAuth.isAuthenticated === true
+//       ? <Component {...props}/>
+//       : <Redirect to='/login' />
+//   )} />
+// )
 
 export default App;
